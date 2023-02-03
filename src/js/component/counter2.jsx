04e74988841533f370d71f00ae9aa84f206310d5
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-02-03 17:30:37 +0000

    new logic added to counter2

--- src/js/component/counter2.jsx
+++ src/js/component/counter2.jsx
@@ -4,69 +4,21 @@ import ClockCard from "./clockCard.jsx";
 
 //create your first component
 const Counter2 = (props) => {
-	let count = String(props.count);
-	let digit1, digit2, digit3, digit4, digit5, digit6;
-
-	if (count.length == 1) {
-		digit1 = '0';
-		digit2 = '0';
-		digit3 = '0';
-		digit4 = '0';
-		digit5 = '0';
-		digit6 = count[0];
-	} else if (count.length == 2) {
-		digit1 = '0';
-		digit2 = '0';
-		digit3 = '0';
-		digit4 = '0';
-		digit5 = count[0];
-		digit6 = count[1];
-	} else if (count.length == 3) {
-		digit1 = '0';
-		digit2 = '0';
-		digit3 = '0';
-		digit4 = count[0];
-		digit5 = count[1];
-		digit6 = count[2];		
-	} else if (count.length == 4) {
-		digit1 = '0';
-		digit2 = '0';
-		digit3 = count[0];
-		digit4 = count[1];
-		digit5 = count[2];
-		digit6 = count[3];			
-	} else if (count.length == 5) {
-		digit1 = '0';
-		digit2 = count[0];
-		digit3 = count[1];
-		digit4 = count[2];
-		digit5 = count[3];
-		digit6 = count[4];	
-	} else if (count.length == 6) {
-		digit1 = count[0];
-		digit2 = count[1];
-		digit3 = count[2];
-		digit4 = count[3];
-		digit5 = count[4];
-		digit6 = count[5];	
-	}
-	/*
-	for (let i = 0; i <= 6; i++) {
-		const element = array[i];
-		
-	}
-	*/
-
-
+	let count = String(props.count);	
+	
+	while (count.length < 6) {
+        count = "0" + count;
+    }
+    
 	return (
 	<div className="container-fluid bg-black d-flex justify-content-center py-5">
 		<ClockCard color={props.color} />
-		<CounterCard2 counter={digit1} color={props.color} />
-		<CounterCard2 counter={digit2} color={props.color}  />
-		<CounterCard2 counter={digit3} color={props.color}  />
-		<CounterCard2 counter={digit4} color={props.color}  />
-		<CounterCard2 counter={digit5} color={props.color}  />
-		<CounterCard2 counter={digit6} color={props.color}  />
+		<CounterCard2 counterDigit={count[0]} color={props.color} />
+		<CounterCard2 counterDigit={count[1]} color={props.color} />
+		<CounterCard2 counterDigit={count[2]} color={props.color} />
+		<CounterCard2 counterDigit={count[3]} color={props.color} />
+		<CounterCard2 counterDigit={count[4]} color={props.color} />
+		<CounterCard2 counterDigit={count[5]} color={props.color} />
 	</div>)
 };
 
--- src/js/component/counterCard2.jsx
+++ src/js/component/counterCard2.jsx
@@ -5,7 +5,7 @@ const CounterCard2 = (props) => {
 	return (
 		<div className="card bg-black m-1 custom-container">
             <div className="card-body border rounded-3 custom-content" style={{background: props.color}}>
-                <h5 className="card-title custom-title">{props.counter}</h5>
+                <h5 className="card-title custom-title">{props.counterDigit}</h5>
             </div>
         </div>
 	);
