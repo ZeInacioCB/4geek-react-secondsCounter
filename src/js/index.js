b860a073fc2742ea8f4417c44ffff4893e2464b9
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-02-03 15:30:25 +0000

    another root added in template.html to experiment with another Counter

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,25 @@ import "../styles/index.css";
 //import your own components
 import Counter from "./component/counter.jsx";
 
-//render your react application
-let counter = 0;
-function renderCounter() {
+//render your react application in App root
+let counter1 = 0;
+function renderCounter1() {
     ReactDOM.render(
-        <Counter count={counter} />, 
+        <Counter count={counter1} />, 
         document.querySelector("#app")
     )
-    counter++;
+    counter1++;
 }
+setInterval(renderCounter1, 1000);
 
-setInterval(renderCounter, 1000);
+
+//render react application in Root root
+let counter2 = 0;
+function renderCounter2() {
+    ReactDOM.render(
+        <Counter count={counter2} />, 
+        document.getElementById('root')
+    )
+    counter2++;
+}
+setInterval(renderCounter2, 500);
